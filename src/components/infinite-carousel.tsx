fc024c1fdc5e3691a9262f4930ee8af5751d6514
Jasper Van Dyck <jasper.vandyck@hotmail.be>
2024-01-21 22:00:52 +0100

    probeersel met gradient carousel

--- src/components/infinite-carousel.tsx
+++ src/components/infinite-carousel.tsx
@@ -2,6 +2,16 @@ export async function InfiniteCarousel() {
   return (
     <div className="flex w-full items-center justify-center">
       <div className="relative w-full max-w-screen-lg overflow-hidden">
+        {/* <div className="pointer-events-none absolute -top-1 z-10 h-20 w-full bg-gradient-to-b from-white to-transparent" />
+        <div className="pointer-events-none absolute -bottom-1 z-10 h-20 w-full bg-gradient-to-t from-white to-transparent" />
+        <div className="pointer-events-none absolute -left-1 z-10 h-full w-20 bg-gradient-to-r from-white to-transparent" />
+        <div className="pointer-events-none absolute -right-1 z-10 h-full w-20 bg-gradient-to-l from-white to-transparent" /> */}
+
+        <div className="corner top-left dark:bg-gradient-dark" />
+        <div className="corner top-right dark:bg-gradient-dark" />
+        <div className="corner bottom-left dark:bg-gradient-dark" />
+        <div className="corner bottom-right dark:bg-gradient-dark" />
+
         <div className="mx-auto grid h-[250px] w-[300px] animate-skew-scroll grid-cols-1 gap-5 sm:w-[600px] sm:grid-cols-2">
           <div className="flex cursor-pointer items-center space-x-2 rounded-md border border-gray-100 p-5 shadow-md transition-all hover:-translate-y-1 hover:translate-x-1 hover:scale-[1.025] hover:shadow-xl">
             <svg
