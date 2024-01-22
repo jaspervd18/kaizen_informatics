b28e50527391f8736e7177993872be35e7183274
Jasper Van Dyck <jasper.vandyck@hotmail.be>
2024-01-22 18:50:11 +0100

    sheet ipv dialog

--- src/components/layout/navbar/mobile-menu.tsx
+++ src/components/layout/navbar/mobile-menu.tsx
@@ -5,13 +5,11 @@ import { usePathname, useSearchParams } from "next/navigation";
 import { Fragment, useEffect, useState } from "react";
 
 import { Menu } from "@/lib/types";
-import { MenuIcon, XIcon } from "lucide-react";
-import { Dialog } from "@/components/ui/dialog";
-import { DialogContent } from "@radix-ui/react-dialog";
+import { MenuIcon, Sheet, XIcon } from "lucide-react";
+import { SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 export default function MobileMenu({ menu }: { menu: Menu[] }) {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
   const [isOpen, setIsOpen] = useState(false);
   const openMobileMenu = () => setIsOpen(true);
   const closeMobileMenu = () => setIsOpen(false);
@@ -28,46 +26,34 @@ export default function MobileMenu({ menu }: { menu: Menu[] }) {
 
   useEffect(() => {
     setIsOpen(false);
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   return (
-    <>
-      <button
-        onClick={openMobileMenu}
+    <Sheet>
+      <SheetTrigger
         aria-label="Open mobile menu"
         className="flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white md:hidden"
       >
         <MenuIcon className="h-4" />
-      </button>
-      <Dialog onOpenChange={closeMobileMenu}>
-        <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
-        <DialogContent className="fixed bottom-0 left-0 right-0 top-0 flex h-full w-full flex-col bg-white pb-6 dark:bg-black">
-          <div className="p-4">
-            <button
-              className="mb-4 flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white"
-              onClick={closeMobileMenu}
-              aria-label="Close mobile menu"
-            >
-              <XIcon className="h-6" />
-            </button>
-
-            {menu.length ? (
-              <ul className="flex w-full flex-col">
-                {menu.map((item: Menu) => (
-                  <li
-                    className="py-2 text-xl text-black transition-colors hover:text-neutral-500 dark:text-white"
-                    key={item.title}
-                  >
-                    <Link href={item.path} onClick={closeMobileMenu}>
-                      {item.title}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            ) : null}
-          </div>
-        </DialogContent>
-      </Dialog>
-    </>
+      </SheetTrigger>
+      <SheetContent>
+        <div className="p-4">
+          {menu.length ? (
+            <ul className="flex w-full flex-col">
+              {menu.map((item: Menu) => (
+                <li
+                  className="py-2 text-xl text-black transition-colors hover:text-neutral-500 dark:text-white"
+                  key={item.title}
+                >
+                  <Link href={item.path} onClick={closeMobileMenu}>
+                    {item.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : null}
+        </div>
+      </SheetContent>
+    </Sheet>
   );
 }
