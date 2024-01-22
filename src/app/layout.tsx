fca248b7e9b27f40638740d2a9eeb4780b9ff13e
Jasper Van Dyck <jasper.vandyck@hotmail.be>
2024-01-22 18:36:19 +0100

    styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,8 @@ export default async function RootLayout({
   children: ReactNode;
 }) {
   return (
-    <html lang="nl" className={GeistSans.variable}>
-      <body className="flex min-h-screen flex-col bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
+    <html lang="nl">
+      <body>
         <Navbar />
         <Suspense>
           <main className="container grow">{children}</main>
